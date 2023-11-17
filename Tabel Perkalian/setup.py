536b0aeed7a7f1412bb56e95819bb74af870216a
Nur Ridwan <130064161+Zxnc0@users.noreply.github.com>
2023-11-17 13:46:25 +0700

    Update setup.py

--- Tabel Perkalian/setup.py
+++ Tabel Perkalian/setup.py
@@ -7,7 +7,7 @@ setup(
     description='Tabel perkalian simpel, yang membuat saya pusing menguploadnya ke pypi',
     author='Nur Ridwan',
     author_email='nurridwan270305@gmail.com',
-    url='https://github.com/Zxnc0/tabel-perkalian',
+    url='https://github.com/Zxnc0/Tabel-Perkalian',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
